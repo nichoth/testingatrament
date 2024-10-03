d6f3de25affd7166f79e804bbe3829eb8e44dd4d
nichoth <nichoth@nichoth.com>
2024-10-03 07:08:55 -0700

    add margin to body

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,24 @@
 import { type FunctionComponent, render } from 'preact'
-import { html } from 'htm/preact'
+import Atrament from '@bicycle-codes/atrament'
+import { html, } from 'htm/preact'
+import { useEffect } from 'preact/hooks'
+import { waitFor } from '@bicycle-codes/dom'
 import { example } from '../src/index.js'
+import './style.css'
 
 example()
 
 const Example:FunctionComponent<unknown> = function () {
-    return html`<div>hello</div>`
+    useEffect(() => {
+        (async () => {
+            const canvas = await waitFor('canvas')
+            console.log('canvas', canvas)
+            const atrament = new Atrament(canvas)
+            console.log('atrrr', atrament)
+        })()
+    }, [])
+
+    return html`<canvas width=400 height=400></canvas>`
 }
 
 render(html`<${Example} />`, document.getElementById('root')!)
